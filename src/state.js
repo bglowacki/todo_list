08c95edd200a56326ff5bca02beaaaa45f578a32
Bartek Glowacki <marathone@gmail.com>
2018-03-08 06:50:27 +0100

    First commit

---
+++ src/state.js
@@ -0,0 +1,10 @@
+let initialState = {
+  message: "Hello World"
+};
+
+export default function(state=initialState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import {createStore} from "redux"
+import state from "./state"
+
+let store = createStore(state);
+
+export default store
+
